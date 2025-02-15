4adcaf329ebf256ec7be4f41ae672152561f71c4
Defiant7426 <pldelacruzv@gmail.com>
2025-02-14 23:41:02 -0500

    Empezando proyecto

---
+++ udemo/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div className="bg-primary-500">
+        Gola
+      </div>
+      <button className="outline-cyan-500 transition hover:outline-2">dfsd</button>
+      <div className="text-primary-300">
+        haa
+      </div>
+
+    </>
+  )
+}
+
+export default App
